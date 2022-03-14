685f4ab788354c97963671e3d5bdb9fa721fe85b
katereclark <cla16022@byui.edu>
2022-03-14 10:25:56 -0600

    Created admin post routes and controllers.

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,77 @@
 const express = require('express');
+const path = require('path');
 const {body, check} = require('express-validator');
  
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
+
+// /admin/add-costume => GET
+router.get('/add-costume', isAuth, adminController.getAddCostume);
+
+// /admin/costumes => GET
+router.get('/costumes', isAuth, adminController.getCostumes);
+
+// /admin/add-costume => POST
+router.post(
+  '/add-costume',
+  [
+    body('category')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('costumeName')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('rentalFee').isFloat(),
+    body('size')
+        .isString()
+        .isLength({ min: 1 })
+        .trim(),
+    body('image').isURL(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postAddCostume
+);
+
+// /admin/edit-costume/:costumeId => GET
+router.get('/edit-costume/:costumeId', isAuth, adminController.getEditCostume);
+
+// /admin/edit-costume => POST
+router.post(
+  '/edit-costume',
+  [
+	body('category')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('costumeName')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('rentalFee').isFloat(),
+    body('size')
+        .isString()
+        .isLength({ min: 1 })
+        .trim(),
+    body('image').isURL(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postEditCostume
+);
+
+// /admin/delete-costume => POST
+router.post('/delete-costume', isAuth, adminController.postDeleteCostume);
+
+
 /**
  * Define a schema for Rentals
  * @swagger
