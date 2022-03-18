a897367eac6581b8478dea5c2cc1c5f5a120d991
zircadia <76924125+zircadia@users.noreply.github.com>
2022-03-18 11:42:07 -0500

    updated route use

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/costumes', inventoryRoutes);
+app.use('/costume', inventoryRoutes);
 app.use('/admin', adminRoutes);
 app.use('/auth', authRoutes);
 
