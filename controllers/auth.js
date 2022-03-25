d03a4fd13a5a02884ca663ba3c0c02afe8f15fd7
zircadia <76924125+zircadia@users.noreply.github.com>
2022-03-24 19:46:01 -0500

    jwt password processing edits

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,98 +88,99 @@ exports.login = async (req, res, next) => {
 
 // TODO: This route works using the token method used in shop instead of jwt/is-pass-auth middleware
 // Reset Password
-exports.postReset = (req, res, next) => {
-  crypto.randomBytes(32, (err, buffer) => {
-    const token = buffer.toString('hex');
-    User.findOne({email: req.body.email})
-      .then(user => {
-        if (!user) {
-          const error = new Error('User not found.');
-          error.statusCode = 404;
-          throw error;
-        }
-      user.resetToken = token;
-      user.resetTokenExpiration = Date.now() + 3600000;
-      return user.save();
-      })
-      .then(result => {
-        //send email? link provided will be http://localhost:3000/reset/${token}
-        res.status(200).json({ 
-          message: 'Password reset request authorized', token: token 
-        });
-    })
-    .catch (err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      return next(err);
-    });
-  });
-};
+// exports.postReset = (req, res, next) => {
+//   crypto.randomBytes(32, (err, buffer) => {
+//     const token = buffer.toString('hex');
+//     User.findOne({email: req.body.email})
+//       .then(user => {
+//         if (!user) {
+//           const error = new Error('User not found.');
+//           error.statusCode = 404;
+//           throw error;
+//         }
+//       user.resetToken = token;
+//       user.resetTokenExpiration = Date.now() + 3600000;
+//       return user.save();
+//       })
+//       .then(result => {
+//         //send email? link provided will be http://localhost:3000/reset/${token}
+//         res.status(200).json({ 
+//           message: 'Password reset request authorized', token: token 
+//         });
+//     })
+//     .catch (err => {
+//       if (!err.statusCode) {
+//         err.statusCode = 500;
+//       }
+//       return next(err);
+//     });
+//   });
+// };
 
 // TODO: This route works using the token method used in shop instead of jwt/is-pass-auth middleware
 //Create new password
-exports.postNewPassword = async (req, res, next) => {
-  const newPassword = req.body.password;
-  const userId = req.body.userId;
-  const passwordToken = req.body.passwordToken;
-  try {
-    const resetUser = await User.findOne({
-      resetToken: passwordToken, 
-      resetTokenExpiration: { $gt: Date.now() },
-      _id: userId
-    });
-    const hashedPassword = await bcrypt.hash(newPassword, 12);
-    resetUser.password = hashedPassword;
-    resetUser.resetToken = undefined;
-    resetUser.resetTokenExpiration = undefined;
-    await resetUser.save();
-    res.status(200).json({ message: 'Password reset successfully', userId: resetUser._id.toString() });
-  } catch {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  }
-};
+// exports.postNewPassword = async (req, res, next) => {
+//   const newPassword = req.body.password;
+//   const userId = req.body.userId;
+//   const passwordToken = req.body.passwordToken;
+//   try {
+//     const resetUser = await User.findOne({
+//       resetToken: passwordToken, 
+//       resetTokenExpiration: { $gt: Date.now() },
+//       _id: userId
+//     });
+//     const hashedPassword = await bcrypt.hash(newPassword, 12);
+//     resetUser.password = hashedPassword;
+//     resetUser.resetToken = undefined;
+//     resetUser.resetTokenExpiration = undefined;
+//     await resetUser.save();
+//     res.status(200).json({ message: 'Password reset successfully', userId: resetUser._id.toString() });
+//   } catch {
+//     if (!err.statusCode) {
+//       err.statusCode = 500;
+//     }
+//     next(err);
+//   }
+// };
 
 // OLDER VERSIONS - WILL WORK WITHOUT MIDDLEWARE BUT NO TOKEN WITH EXPIRATION GETS PASSED
 
 // // TODO: Older version - This route works if not using the is-pass-auth middleware
 // // Reset Password
-// exports.postReset = async (req, res, next) => {
-//   let loadedUser;
-//     try {
-//       const user = await User.findOne({email: req.body.email});
-//       if (!user) {
-//         const error = new Error('User not found.');
-//         error.statusCode = 404;
-//         throw error;
-//       }
-//       loadedUser = user; 
-//       const token = jwt.sign(
-//         { 
-//           email: loadedUser.email, 
-//           userId: loadedUser._id.toString()
-//         }, 
-//         `secretpasswordsauce`, 
-//         { expiresIn: '20m' }
-//       );
+exports.postReset = async (req, res, next) => {
+  let loadedUser;
+    try {
+      console.log(req.body.email);
+      const user = await User.findOne({email: req.body.email});
+      if (!user) {
+        const error = new Error('User not found.');
+        error.statusCode = 404;
+        throw error;
+      }
+      loadedUser = user; 
+      const token = jwt.sign(
+        { 
+          email: loadedUser.email, 
+          userId: loadedUser._id.toString()
+        }, 
+        'secretpasswordsauce', 
+        { expiresIn: '20m' }
+      );
 
-//       //send email? link provided will be http://localhost:3000/reset/${token}
+      //send email? link provided will be http://localhost:3000/reset/${token}
 
-//       res.status(200).json({ message: 'Password reset request authorized', token: token, userId: loadedUser._id.toString() });
-//     } catch (err) {
-//       if (!err.statusCode) {
-//         err.statusCode = 500;
-//       }
-//       next(err);
-//     }
-//   }
+      res.status(200).json({ message: 'Password reset request authorized', token: token, userId: loadedUser._id.toString() });
+    } catch (err) {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    }
+  }
 
 
-// // TODO: This route currently does not work. Do we even really need it if it is for the frontend? 
-// // Prior to rendering reset password form, frontend is verifying status of url parameter, :tokenId
+// TODO: This route currently does not work. Do we even really need it if it is for the frontend? 
+// Prior to rendering reset password form, frontend is verifying status of url parameter, :tokenId
 // exports.isPassLinkAuth = async (req, res, next) => {
 //   const token = tokenId;
 //   const userId  = await jwt.decode(token.userId);
@@ -208,24 +209,45 @@ exports.postNewPassword = async (req, res, next) => {
 //     }
 //   };
 
-// // TODO: Older version - This route works if not using the is-pass-auth middleware
-// //Create new password
-// exports.postNewPassword = async (req, res, next) => {
-//   const newPassword = req.body.password;
-//   const userId = req.body.userId;
-//   try {
-//     const resetUser = await User.findOne({
-//       _id: userId
-//     });
-//     const hashedPassword = await bcrypt.hash(newPassword, 12);
-//     resetUser.password = hashedPassword;
+// TODO: Older version - This route works if not using the is-pass-auth middleware
+//Create new password
+exports.postNewPassword = async (req, res, next) => {
+  const authHeader = req.get('Authorization');
+    if (!authHeader) {
+      const error = new Error('Not authenticated - missing Authentication Headers.');
+      error.statusCode = 401;
+      throw error;
+    }
+  try { 
+    const token = authHeader.split(' ')[1];
+    let decodedToken;
+    try { decodedToken = jwt.verify(token, 'secretpasswordsauce')}
+    catch (err) {
+      err.statusCode = 500;
+      throw err;}
+    if (!decodedToken) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+    }
+    const userId  = decodedToken.userId;
+        const passUser = await User.findById(userId);
+        if(!passUser) {
+          console.log(userId);
+          const error = new Error('No user found.');
+          error.statusCode = 404;
+          throw error;
+        }
+    const newPassword = req.body.password;
+    const hashedPassword = await bcrypt.hash(newPassword, 12);
+    passUser.password = hashedPassword;
 
-//     await resetUser.save();
-//     res.status(200).json({ message: 'Password reset successfully', userId: resetUser._id.toString() });
-//   } catch {
-//     if (!err.statusCode) {
-//       err.statusCode = 500;
-//     }
-//     next(err);
-//   }
-// };
+    await passUser.save();
+    res.status(200).json({ message: 'Password reset successfully', userId: userId });
+  } catch(err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -8,8 +8,8 @@ const {
 
 const User = require('../models/user');
 const authController = require('../controllers/auth');
-// const isAuth = require('../middleware/is-auth');
-// const isPassAuth = require('../middleware/is-pass-auth');
+const isAuth = require('../middleware/is-auth');
+const isPassAuth = require('../middleware/is-pass-auth');
 
 const router = express.Router();
 
