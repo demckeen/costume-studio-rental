5d8a8bbc3ade3585eef4d4c3bcff8dd3cdef9463
zircadia <76924125+zircadia@users.noreply.github.com>
2022-03-29 17:44:54 -0500

    is-auth fixes

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -2,13 +2,22 @@
 
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-  const authHeader = req.get('Authorization');
+module.exports = async (req, res, next) => {
+  let authHeader;
+
+  try {
+   authHeader = await req.get('Authorization');
+
   if (!authHeader) {
     const error = new Error('Not authenticated.');
     error.statusCode = 401;
     throw error;
+  }}
+  catch(err) {if (!err.statusCode) {
+    err.statusCode = 500;
   }
+  next(err);
+  }  
   const token = authHeader.split(' ')[1];
   let decodedToken;
   try {
